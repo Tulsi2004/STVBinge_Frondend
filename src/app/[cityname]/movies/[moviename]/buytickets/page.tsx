3805deef7236f3e4d1c453b54bfd1a0ef9938373
Tulsi2004 <tulsimani04@gmail.com>
2024-09-02 23:19:13 +0530

    frontend day3

--- src/app/[cityname]/movies/[moviename]/buytickets/page.tsx
+++ src/app/[cityname]/movies/[moviename]/buytickets/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import React from 'react'
-import DatePicker from "react-horizontal-datepicker";
+//import DatePicker from "react-horizontal-datepicker";
 import './BuyTicketsPage.css'
 import Link from 'next/link';
 import { usePathname, useParams } from 'next/navigation';
 
 const page = () => {
+    const pathname = usePathname();
 
     const movie={
         moviename:"Despicable-me-4",
@@ -28,21 +29,31 @@ const page = () => {
         ]
     }
 
-
-
-
     return(
         <div className='buytickets'>
             <div className='s1'>
                 <div className='head'>
                     <h1>{movie.moviename} - {movie.language} </h1>
                     <h3> {movie.type} </h3>
+                    //Date Picker function here
                 </div>
   
-
-
             </div> 
-            <div className='screens'></div>
+            <div className='screens'>
+                {
+                    movie.screens.map((screen,index)=>{
+                        return(
+                            <div className='screen' key={index}>
+                                <div>
+                                    <h2>{screen.name}</h2>
+                                    <h3>{screen.location}</h3>
+                                </div>
+                                <Link href={`${pathname}/P{screen.name}`} className='theme_btn1 linkstylenone'>Select</Link>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
 import './Navbar.css'
@@ -6,8 +8,11 @@ import { FaUserCircle } from "react-icons/fa";
 import { RiArrowDropDownFill } from 'react-icons/ri'
 import logo from '@/assets/STV_Logo.png'
 import Image from 'next/image'
+import LocationPopup from '@/popups/location/LocationPopup';
 
 const Navbar = () => {
+    const [showLocationPopup, setShowLocationPopup] = React.useState<boolean>(false)
+
     return (
         <nav>
             <div className='left'>
@@ -20,13 +25,21 @@ const Navbar = () => {
                 </div>
             </div>
             <div className='right'>
-                <p className='dropdown'>Mumbai<RiArrowDropDownFill className="dropicon" /></p>
+                <p className='dropdown'
+                onClick={()=> setShowLocationPopup(true)}
+                >Mumbai<RiArrowDropDownFill className="dropicon" /></p>
                 <button className='theme_btn1 linkstylenone' >Logout</button>
                 <Link href="/" className='theme btn_1 linkstylenone'></Link>
                 <Link href="/" className='linkstylenone'>
                 <FaUserCircle  className='theme icon_1'/>
                 </Link>
             </div>
+            {
+                showLocationPopup &&
+                <LocationPopup
+                    setShowLocationPopup={setShowLocationPopup}
+                />
+            }
         </nav>
     )
 }
